2430ac62876495de737569b87103fce26a031125
divyansh-tripathi <divyanshtripathi201@gmail.com>
2025-09-25 00:03:52 +0530

    change index.js

--- backend/index.js
+++ backend/index.js
@@ -280,23 +280,14 @@ const fetchUser = async (req,res,next)=>{
 
 
 // creating endpoint for adding products in cartdata
-app.post("/addtocart", fetchUser, async (req, res) => {
-  console.log("Added", req.body.itemId);
 
-  let userData = await Users.findOne({ _id: req.user.id });
-
-  if (!userData.cartData[req.body.itemId]) {
-    userData.cartData[req.body.itemId] = 0;
-  }
-
-  userData.cartData[req.body.itemId] += 1;
-
-  await Users.findOneAndUpdate(
-    { _id: req.user.id },
-    { cartData: userData.cartData }
-  );
-
-  res.json({ message: "Added" });
+app.post("/addtocart",fetchUser, async (req, res) => {
+  console.log("Added",req.body.itemId)
+  // console.log(req.body,req.user);
+  let userData = await Users.findOne({_id:req.user.id})
+  userData.cartData[req.body.itemId] += 1
+  await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData})
+res.send('Added')
 });
 
 
